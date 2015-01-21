a6f82f194de5fb858093310c72eb05eb030050ce
sebastien-j <sebastien.jean@mail.mcgill.ca>
2015-01-20 20:26:58 -0500

    Fix file listing

--- extract_features.py
+++ extract_features.py
@@ -99,7 +99,7 @@ def main():
     data = []
 
     root_dir = args.root_dir
-    for dir in sorted(root_dir):
+    for dir in sorted(os.listdir(root_dir)):
         for file in sorted(os.listdir(os.path.join(root_dir,dir))):
             data.append(imread(os.path.join(root_dir,dir,file))[v_min:v_max,h_min:h_max])
 
